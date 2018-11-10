eea9b46f50c551fae5f66815c1b40ffaef0c3359
Loori-R <Loor@list.ru>
2018-11-10 23:46:28 +0300

    feat:add close when you click on the cross

--- notifications/script.js
+++ notifications/script.js
@@ -1,2 +1,7 @@
 const notice = document.querySelector('aside.notifications')
+const close = document.getElementsByClassName('close')
 setTimeout(()=>{notice.style.opacity = '1'},5000)
+close[0].onclick = ()=>
+{
+    notice.style.opacity = '0'
+}
