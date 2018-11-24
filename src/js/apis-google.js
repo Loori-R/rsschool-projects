c893858da9e7cd513a335ebf586463409b8932f3
Loori-R <Loor@list.ru>
2018-11-24 17:23:37 +0300

    feat: add gapi client

--- src/js/apis-google.js
+++
@@ -1,5 +0,0 @@
-module.exports = function load_api_client() {
-    const script = document.createElement('script');
-    script.src = "https://apis.google.com/js/client.js?onload=init"
-    document.body.appendChild(script);
-}
