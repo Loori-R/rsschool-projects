266fdff592289fa0c4d43e49734476d0f3acd9c0
Loori-R <Loor@list.ru>
2018-11-28 01:29:33 +0300

    feat:request a token for the next pack of video

---
+++ src/js/pageToken.js
@@ -0,0 +1,70 @@
+import {
+    btn
+} from './search-form.js';
+import {
+    loadng,
+    container,
+    dots_block,
+    block_button,
+    next_page,
+    prev_page,
+    count_page
+} from './htmlElem.js';
+import {
+    searchRes
+} from './search-result.js';
+import {
+    createDots
+} from './createDots.js';
+import {
+    createVideoCard
+} from './create-result.js'
+import {
+    res_data
+} from './searchClick.js'
+import {
+    count_video_in_page
+} from './createDots.js'
+
+let count_click_token = 1
+
+export function getPrevPage(e) {
+    e.preventDefault();
+    if (count_click_token === 1) {
+        prev_page.disabled = true
+    }
+    count_click_token--
+    next_page.disabled = false
+    document.body.appendChild(loadng)
+    btn.disabled = true
+    container.innerHTML = ""
+    dots_block.innerHTML = ""
+    count_page.innerText = `${count_click_token} of ${res_data[1].totalPageToken}`
+    searchRes(res_data, res_data[1].prevPageToken)
+    setTimeout(() => {
+        createDots()
+        createVideoCard(1, +count_video_in_page + 1)
+        btn.disabled = false
+    }, 1000)
+}
+export function getNextPage(e) {
+    e.preventDefault();
+    if (count_click_token === res_data[1].totalPageToken) {
+        next_page.disabled = true
+    }
+    count_click_token++
+    prev_page.disabled = false
+    document.body.appendChild(loadng)
+    btn.disabled = 'true'
+    container.innerHTML = ""
+    dots_block.innerHTML = ""
+    count_page.innerText = `${count_click_token} of ${res_data[1].totalPageToken}`
+    block_button.appendChild(count_page)
+    block_button.appendChild(prev_page)
+    searchRes(res_data, res_data[1].nextPageToken)
+    setTimeout(() => {
+        createDots()
+        createVideoCard(1, +count_video_in_page + 1)
+        btn.disabled = false
+    }, 1000)
+}
