a0c501a27dc6897acba70bc004969cbea5bbfbbf
Loor <loor@list.ru>
2018-11-27 16:46:45 +0300

    feat: add return pageToken

--- src/js/search-result.js
+++ src/js/search-result.js
@@ -1,33 +1,37 @@
-import{query} from './search-form.js'; 
-export function search_res (res_data,callback){
-    const api_key = 'AIzaSyDVdE4LwpceNaEd3UTHg4ESHrBjnWwne_E'  
+import { query } from './search-form.js';
+export function search_res(res_data, pageToken) {
+    const api_key = 'AIzaSyDVdE4LwpceNaEd3UTHg4ESHrBjnWwne_E'
     const request = gapi.client.youtube.search.list({
-    q: query.value,
-    part: 'snippet',
-    order: 'viewCount',
-    type: 'video',
-    maxResults: 15
+        q: query.value,
+        part: 'snippet',
+        order: 'viewCount',
+        type: 'video',
+        pageToken: pageToken,
+        maxResults: 15
     });
     request.execute((response) => {
         const results = response.result;
-        results.items.forEach((item, i) => { 
+        results.items.forEach((item, i) => {
             const xhr = new XMLHttpRequest();
             xhr.open('GET', `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${item.id.videoId}&key=${api_key}`, true);
             xhr.send();
             xhr.onreadystatechange = () => {
-            if (xhr.readyState != 4) {return}
-            else{   
-        res_data[i+1] = {          
-            link:`https://www.youtube.com/watch?v=${item.id.videoId}`,
-            title:item.snippet.title,
-            thumb:item.snippet.thumbnails.medium.url,
-            descr:item.snippet.description,
-            author:item.snippet.channelTitle,
-            view:JSON.parse(xhr.response).items[0].statistics.viewCount,
-            date:item.snippet.publishedAt.match(/\d+-\d+-\d+/).join()
-            }}                                       
-            }           
-        });              
-    }); 
-return res_data
+                if (xhr.readyState != 4) { return }
+                else {
+                    res_data[i + 1] = {
+                        link: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+                        title: item.snippet.title,
+                        thumb: item.snippet.thumbnails.medium.url,
+                        descr: item.snippet.description,
+                        author: item.snippet.channelTitle,
+                        view: JSON.parse(xhr.response).items[0].statistics.viewCount,
+                        date: item.snippet.publishedAt.match(/\d+-\d+-\d+/).join(),
+                        nextPageToken: results.nextPageToken,
+                        prevPageToken: results.prevPageToken
+                    }
+                }
+            }
+        });
+    });
+    return res_data
 }
