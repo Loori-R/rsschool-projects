2f14cd1cac54e6b274d0b3b3b5c81aa4233e1386
Loori-R <Loor@list.ru>
2019-02-10 20:08:35 +0300

    feat: add template  main menu

---
+++ final-game/src/screens/home/template/menu.template.js
@@ -0,0 +1,6 @@
+export const menu = `<h1 class="name-game">tale dragon</h1>
+<div class="main-menu">
+<div class="elem-main-menu" id="about">Музыка</div>
+<div class="elem-main-menu" id="play">Играть</div>
+<div class="elem-main-menu" id="exit"><a href='../dist/'>Выйти</a></div>
+</div>`;
