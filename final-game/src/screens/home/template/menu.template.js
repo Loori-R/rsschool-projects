33ca15a078b04c1c7ce743f1d3b941febea068bc
Loori-R <Loor@list.ru>
2019-02-10 20:20:25 +0300

    fix:change link to exit

--- final-game/src/screens/home/template/menu.template.js
+++ final-game/src/screens/home/template/menu.template.js
@@ -2,5 +2,5 @@ export const menu = `<h1 class="name-game">tale dragon</h1>
 <div class="main-menu">
 <div class="elem-main-menu" id="about">Музыка</div>
 <div class="elem-main-menu" id="play">Играть</div>
-<div class="elem-main-menu" id="exit"><a href='../dist/'>Выйти</a></div>
+<div class="elem-main-menu" id="exit"><a href='https://loori-r.github.io/final_game/'>Выйти</a></div>
 </div>`;
